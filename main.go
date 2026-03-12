83ab75a808e3f07f083ef1dc5ef6b3f6b0c6c297
AKSHAYK0UL <akshaykoul999+Github@gmail.com>
2026-03-12 10:36:32 +0530

    add os.Getenv("PORT")

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
@@ -11,8 +12,15 @@ func main() {
 
 	mux.HandleFunc("GET /ws", rm.HandleWebSocket)
 	mux.HandleFunc("GET /", handleHome)
+
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8080"
+	}
+
 	log.Println("Server started on :8080")
-	log.Fatal(http.ListenAndServe(":8080", mux))
+	log.Fatal(http.ListenAndServe(":"+port, mux))
 }
 
 func handleHome(w http.ResponseWriter, r *http.Request) {
